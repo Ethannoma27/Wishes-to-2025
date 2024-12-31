11d3e04d93c6aebddb47f3f715428b5394c7fb1e
Ethannoma27 <Ethannoma27@hotmail.com>
2024-12-31 22:26:33 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,65 @@
-// 烟花效果
-const fireworksContainer = document.querySelector('.fireworks');
+// 烟花动画代码
+const canvas = document.getElementById('fireworks');
+const ctx = canvas.getContext('2d');
 
-// 创建烟花动画
-function createFirework() {
-  const firework = document.createElement('div');
-  firework.classList.add('firework');
-  const size = Math.random() * 5 + 10;  // 随机生成烟花大小
-  firework.style.width = `${size}px`;
-  firework.style.height = `${size}px`;
-  firework.style.top = `${Math.random() * 100}%`;
-  firework.style.left = `${Math.random() * 100}%`;
-  fireworksContainer.appendChild(firework);
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-  // 动画结束后移除烟花
-  setTimeout(() => {
-    firework.remove();
-  }, 1000);
+let particles = [];
+
+function Particle(x, y, color, size, speed) {
+    this.x = x;
+    this.y = y;
+    this.color = color;
+    this.size = size;
+    this.speed = speed;
+    this.angle = Math.random() * 2 * Math.PI;
+    this.velocityX = Math.cos(this.angle) * speed;
+    this.velocityY = Math.sin(this.angle) * speed;
+    this.life = 100;
 }
 
-// 每隔200ms创建一个烟花
-setInterval(createFirework, 200);
+Particle.prototype.update = function() {
+    this.x += this.velocityX;
+    this.y += this.velocityY;
+    this.size *= 0.96;
+    this.life -= 2;
+};
+
+Particle.prototype.draw = function() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+};
+
+function createFirework(x, y) {
+    const colors = ['#FF4500', '#FF6347', '#FFD700', '#FFFF00'];
+    for (let i = 0; i < 100; i++) {
+        particles.push(new Particle(x, y, colors[Math.floor(Math.random() * colors.length)], 3 + Math.random() * 3, 3 + Math.random() * 3));
+    }
+}
+
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    particles.forEach((particle, index) => {
+        particle.update();
+        particle.draw();
+        if (particle.life <= 0) {
+            particles.splice(index, 1);
+        }
+    });
+
+    requestAnimationFrame(animate);
+}
+
+// 在页面加载时生成烟花
+setInterval(() => {
+    const x = Math.random() * canvas.width;
+    const y = Math.random() * canvas.height / 2;
+    createFirework(x, y);
+}, 1000);
+
+animate();
+
