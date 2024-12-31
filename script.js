6476f73c27bff76782ef0d5bdfbe6281447fae4d
Ethannoma27 <Ethannoma27@hotmail.com>
2024-12-31 21:35:35 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// 简单的烟花效果，可以用粒子动画或GIF
+document.addEventListener('DOMContentLoaded', function() {
+    const fireworks = document.getElementById('fireworks');
+    fireworks.style.animation = 'fireworks 2s infinite';
+});
